b013ed19729aed1a25ff073e0808551c388cb286
Sam Turner <samturner.23@me.com>
2017-11-28 12:30:02 +1100

    * Fixed up spacing formatting * Added a mechanism for passing GlobalOptions to a  Vue-Highcharts component and calling setOptions

--- src/VueHighcharts.vue
+++ src/VueHighcharts.vue
@@ -8,74 +8,88 @@
   import {warn} from './debug'
 
   export default {
-    props: ['options', 'Highcharts'],
-    name: 'VueHighcharts',
-    data () {
-      return {
-        chart: null
-      }
-    },
-    mounted(){
-      if (!this.getChart() && this.options) {
-        this.init();
-      }
-    },
+		name: 'VueHighcharts',
 
-    methods: {
-      getChart(){
-        return this.chart
-      },
-      addSeries(options){
-        this.delegateMethod('addSeries', options);
-      },
-      removeSeries(){
-        while(this.getChart().series.length !== 0) {
-          this.getChart().series[0].remove();
-        }
-      },
-      mergeOption(options){
-        this.delegateMethod('update', options)
-      },
-      showLoading(txt){
-        this.getChart().showLoading(txt);
-      },
-      hideLoading(){
-        this.getChart().hideLoading();
-      },
-      delegateMethod(name, ...args){
-        if (!this.getChart()) {
-          warn(`Cannot call [$name] before the chart is initialized. Set prop [options] first.`, this)
-          return
-        }
-        return this.getChart()[name](...args)
-      },
+		props: ['options', 'Highcharts', 'globalOptions'],
 
-      init(){
-        if (!this.getChart() && this.options) {
-         let _Highcharts = this.Highcharts || Highcharts;
-         if (_Highcharts.product == 'Highstock') {
-           this.chart = new _Highcharts.stockChart(this.$el, this.options);
-         } else {
-           this.chart = new _Highcharts.Chart(this.$el, this.options);
-         }
-        }
-      }
-    },
+		data () {
+		  return {
+				chart: null
+		  }
+		},
 
-    watch: {
-      options: function (options) {
-        if (!this.getChart() && options) {
-          this.init()
-        } else {
-          this.getChart().update(this.options);
-        }
-      }
-    },
+		mounted () {
+		  if (!this.getChart() && this.options) {
+				this.init();
+		  }
+		},
 
-    beforeDestroy(){
-      if (this.getChart()) {
-        this.getChart().destroy();
-      }
-    }
+		methods: {
+		  getChart() {
+				return this.chart
+		  },
+
+		  addSeries (options) {
+				this.delegateMethod('addSeries', options);
+		  },
+
+		  removeSeries () {
+				while(this.getChart().series.length !== 0) {
+				  this.getChart().series[0].remove();
+				}
+		  },
+
+		  mergeOption (options) {
+				this.delegateMethod('update', options)
+		  },
+
+		  showLoading (txt) {
+				this.getChart().showLoading();
+		  },
+
+		  hideLoading () {
+				this.getChart().hideLoading();
+		  },
+
+		  delegateMethod (name, ...args) {
+				if (!this.getChart()) {
+				  warn(`Cannot call [$name] before the chart is initialized. Set prop [options] first.`, this)
+				  return
+				}
+				return this.getChart()[name](...args)
+		  },
+
+		  init () {
+				if (!this.getChart() && this.options) {
+					 let _Highcharts = this.Highcharts || Highcharts;
+
+					 if (this.globalOptions) {
+						 	_Highcharts.setOptions(this.globalOptions)
+					 }
+
+					 if (_Highcharts.product == 'Highstock') {
+					   this.chart = new _Highcharts.stockChart(this.$el, this.options);
+					 } else {
+					   this.chart = new _Highcharts.Chart(this.$el, this.options);
+					 }
+				}
+		  }
+		},
+
+		watch: {
+		  options: function (options) {
+				if (!this.getChart() && options) {
+				  this.init()
+				} else {
+				  this.getChart().update(this.options);
+				}
+		  }
+		},
+
+		beforeDestroy(){
+		  if (this.getChart()) {
+				this.getChart().destroy();
+		  }
+		}
   }
 </script>
